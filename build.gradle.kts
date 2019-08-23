2b4e86fa883e1b87256e45edf2acc2e81254589b
sriram sundharesan <sriram.sundhar@gmail.com>
2019-08-23 17:36:20 -0400

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+import com.google.protobuf.gradle.*
+import org.gradle.kotlin.dsl.provider.gradleKotlinDslOf
+
+plugins {
+    java
+    idea
+    id("com.google.protobuf") version "0.8.8"
+    id("net.researchgate.release") version "2.8.0"
+    `java-library`
+}
+group = "$group"
+version = "$version"
+
+repositories {
+    jcenter()
+    maven("https://plugins.gradle.org/m2/")
+}
+
+dependencies {
+    compile("com.google.protobuf:protobuf-java:3.6.1")
+    compile("io.grpc:grpc-stub:1.15.1")
+    compile("io.grpc:grpc-protobuf:1.15.1")
+
+    protobuf(files("lib/protos.tar.gz"))
+    testCompile("junit:junit:4.12")
+    testProtobuf(files("lib/protos-test.tar.gz"))
+
+}
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.6.1"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.15.1"
+        }
+    }
+    generateProtoTasks {
+        ofSourceSet("main").forEach {
+            it.plugins {
+                id("grpc")
+            }
+        }
+    }
+}
